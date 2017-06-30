e3cd09cb9e640eccda57d2d83d394602afc41743
Pierre Manceron <pierre.manceron@gmail.com>
2017-06-30 17:31:53 +0200

    Rename to vinci

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-setup(name='keras-rl',
+setup(name='vinci',
       version='0.3.1~dev',
       description='Deep Reinforcement Learning for Keras',
       author='Matthias Plappert, Pierre Manceron',
-      author_email='matthiasplappert@me.com',
-      url='https://github.com/matthiasplappert/keras-rl',
-      download_url='https://github.com/matthiasplappert/keras-rl/archive/v0.3.0.tar.gz',
+      url='https://github.com/phylliade/vinci',
       license='MIT',
       install_requires=['keras>=1.0.7'],
       extras_require={
