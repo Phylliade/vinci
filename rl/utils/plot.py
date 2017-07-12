63c2edcc151d4bc00fbfa2fe36161f1e9cc96ea2
Pierre Manceron <pierre.manceron@gmail.com>
2017-07-12 18:24:05 +0200

    Consolidate plot_actor + Add plot_critic

--- rl/utils/plot.py
+++ rl/utils/plot.py
@@ -4,13 +4,39 @@ import matplotlib.pyplot as plt
 
 def portrait_actor(actor, definition=100, plot=True):
     portrait = np.zeros((definition, definition))
-    for index_x, x in enumerate(np.linspace(-1, 0.6, num=definition)):
-        for index_v, v in enumerate(np.linspace(-1, 1, num=definition)):
-            portrait[index_x, index_v] = actor.predict([x, v])
+    center = -0.523
+    pos_min = -1.2 - center
+    pos_max = 0.6 - center
+    max_speed = 0.07
+    x_axis = np.linspace(pos_min, pos_max, num=definition)
+
+    for index_x, x in enumerate(x_axis):
+        for index_v, v in enumerate(np.linspace(-max_speed, max_speed, num=definition)):
+            portrait[index_v, index_x] = actor.predict(np.array([[x, v]]))
     if plot:
         plt.figure(figsize=(10, 10))
-        plt.imshow(portrait, cmap="inferno")
+        plt.imshow(portrait, cmap="inferno", extent=[pos_min, pos_max, -max_speed, max_speed], aspect='auto')
         plt.colorbar()
-        plt.scatter([definition / 2], [definition / 2])
+        plt.scatter([0], [0])
+        plt.xlabel("Position")
+        plt.ylabel("Velocity")
+
+
+def portrait_critic(critic, definition=100, plot=True, action=[-1]):
+    portrait = np.zeros((definition, definition))
+    center = -0.523
+    pos_min = -1.2 - center
+    pos_max = 0.6 - center
+    max_speed = 0.07
+    x_axis = np.linspace(pos_min, pos_max, num=definition)
+
+    for index_x, x in enumerate(x_axis):
+        for index_v, v in enumerate(np.linspace(-max_speed, max_speed, num=definition)):
+            portrait[index_v, index_x] = critic.predict_on_batch([np.array(action), np.array([[x, v]])])
+    if plot:
+        plt.figure(figsize=(10, 10))
+        plt.imshow(portrait, cmap="inferno", extent=[pos_min, pos_max, -max_speed, max_speed], aspect='auto')
+        plt.colorbar()
+        plt.scatter([0], [0])
         plt.xlabel("Position")
         plt.ylabel("Velocity")
