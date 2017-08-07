a71a84e3acbc60acd130976f1972cabe1028e290
Pierre Manceron <pierre.manceron@gmail.com>
2017-08-07 16:54:16 +0200

    RTD compatiblity

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy', 'keras', 'tensorflow', 'matplotlib', 'matplotlib.pyplot', 'keras.backend', 'keras.callbacks', 'keras.layers']
+MOCK_MODULES = ['numpy', 'keras', 'tensorflow', 'matplotlib', 'matplotlib.pyplot', 'keras.backend', 'keras.callbacks', 'keras.layers', 'keras.utils']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
