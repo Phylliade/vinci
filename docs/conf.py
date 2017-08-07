47875a1d82e28c8cc447bd0d85c102bcdf58b64a
Pierre Manceron <pierre.manceron@gmail.com>
2017-08-07 16:44:40 +0200

    Use default theme

--- docs/conf.py
+++ docs/conf.py
@@ -126,7 +126,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 # html_theme = 'bootstrap'
 # html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
