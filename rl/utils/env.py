f99024288f911f987a100d8601dc37d2c24c27de
Pierre Manceron <pierre.manceron@gmail.com>
2017-08-08 15:39:35 +0200

    Fix rl/utils/env function

--- rl/utils/env.py
+++ rl/utils/env.py
@@ -4,10 +4,10 @@ import keras
 def populate_env(env):
     # Restrict to euclidian spaces
     env.action_space.dim = env.action_space.shape[0]
-    env.state_space_dim = env.observation_space.shape[0]
+    env.observation_space.dim = env.observation_space.shape[0]
 
     # Create the placeholders for the state and action
     # These are used as inputs for the Deep Networks, in keras
-    env.state = keras.layers.Input(shape=(env.state_space.dim,), name="state")
+    env.state = keras.layers.Input(shape=(env.observation_space.dim,), name="state")
     env.action = keras.layers.Input(shape=(env.action_space.dim,), name="action")
     return(env)
