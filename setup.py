eb8b466957616ebae3d135a5458f540b08d73010
Pierre Manceron <pierre.manceron@gmail.com>
2017-08-11 12:21:30 +0200

    Add dependency in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import find_packages
 
 setup(name='vinci',
       version='1.0.0~dev',
-      description='Deep Reinforcement Learning for Keras',
-      author='Matthias Plappert, Pierre Manceron',
+      description='Deep Reinforcement Learning framework',
+      author='Pierre Manceron',
       url='https://github.com/phylliade/vinci',
       license='MIT',
-      install_requires=['keras>=1.0.7'],
+      install_requires=['keras>=1.0.7', 'seaborn'],
       extras_require={
           'gym': ['gym'],
       },
