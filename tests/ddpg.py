944214b1af029b20baf9b9da9cc713558fec5ff2
Pierre Manceron <pierre.manceron@gmail.com>
2017-11-02 16:29:31 +0100

    Fix ddpg test

--- tests/ddpg.py
+++ tests/ddpg.py
@@ -4,37 +4,42 @@ from rl.random import OrnsteinUhlenbeckProcess
 from rl.utils.networks import simple_actor, simple_critic
 import gym
 from rl.utils.env import populate_env
+from rl.runtime.experiment import DefaultExperiment
 
+# Experiment
+experiment = DefaultExperiment()
 
-# Get the environment
-# And populate it with useful metadata
-env = populate_env(gym.make("MountainCarContinuous-v0"))
+with experiment:
+    # Get the environment
+    # And populate it with useful metadata
+    env = populate_env(gym.make("MountainCarContinuous-v0"))
 
-# Build the actor and the critic
-actor = simple_actor(env)
-critic = simple_critic(env)
+    # Build the actor and the critic
+    actor = simple_actor(env)
+    critic = simple_critic(env)
 
-# Memory
-memory = SimpleMemory(env=env, limit=1000000)
+    # Memory
+    memory = SimpleMemory(env=env, limit=1000000)
 
-# Noise
-random_process = OrnsteinUhlenbeckProcess(
-    size=env.action_space.dim, theta=.15, mu=0., sigma=3.)
+    # Noise
+    random_process = OrnsteinUhlenbeckProcess(
+        size=env.action_space.dim, theta=.15, mu=0., sigma=3.)
 
-# Agent
-agent = DDPGAgent(
-    actor=actor,
-    critic=critic,
-    env=env,
-    memory=memory,
-    random_process=random_process
-)
-agent.compile()
+    # Agent
+    agent = DDPGAgent(
+        experiment=experiment,
+        actor=actor,
+        critic=critic,
+        env=env,
+        memory=memory,
+        random_process=random_process
+    )
+    agent.compile()
 
-agent.train(
-    env=env,
-    nb_episodes=1,
-    visualize=False,
-    verbose=2,
-    nb_max_episode_steps=200,
-    plots=False)
+    agent.train(
+        env=env,
+        nb_episodes=1,
+        visualize=False,
+        verbose=2,
+        nb_max_episode_steps=200,
+        plots=False)
